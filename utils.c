6f5f79fd21a162237e4800d49c04b292cd49e7ec
Igor Martayan <igor@martayan.org>
2021-06-23 14:59:22 +0200

    Add utils

---
+++ utils.c
@@ -0,0 +1,4 @@
+#include "utils.h"
+#include <stdlib.h>
+
+int width(int l1, int l2) { return (abs(l1 - l2) >> 1) + 3; }
---
+++ utils.h
@@ -0,0 +1,6 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+int width(int l1, int l2);
+
+#endif /* __UTILS_H__ */
