2635eab37063f4fb9d2b94d1292bba4e3adb54ab
Igor Martayan <igor@martayan.org>
2021-06-24 10:20:57 +0200

    Larger width for standard alignment

--- utils.c
+++ utils.c
@@ -2,3 +2,4 @@
 #include <stdlib.h>
 
 int width(int l1, int l2) { return (abs(l1 - l2) >> 1) + 3; }
+int width_large(int l1, int l2) { return abs(l1 - l2) + 5; }
--- utils.h
+++ utils.h
@@ -2,5 +2,6 @@
 #define __UTILS_H__
 
 int width(int l1, int l2);
+int width_large(int l1, int l2);
 
 #endif /* __UTILS_H__ */
