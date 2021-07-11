4b32c7a493d59a97cad6edd8fd4e4583ba3708b6
Igor Martayan <igor@martayan.org>
2021-07-11 12:58:39 +0200

    Unify width computation

--- utils.c
+++ utils.c
@@ -1,5 +1,10 @@
 #include "utils.h"
 #include <stdlib.h>
 
-int width(int l1, int l2) { return (abs(l1 - l2) >> 1) + 3; }
-int width_large(int l1, int l2) { return abs(l1 - l2) + 5; }
+int width(int l1, int l2) {
+  int lmax = l1 > l2 ? l1 : l2;
+  int lmin = l1 < l2 ? l1 : l2;
+  int diff = (lmax - lmin + 1) >> 1;
+  int margin = 3 + (lmax >> 7);
+  return diff + margin;
+}
